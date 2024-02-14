ff95a148fa0e6a4fb26f9d7c641bc4b792edddcf
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-14 19:46:12 -0300

    implementação da rota de criação da tarefa

--- server.js
+++ server.js
@@ -27,6 +27,18 @@ app.get('/tasks', (req, res) => {
     });
 });
 
+app.post('/tasks', (req, res) => {
+    const {name, status, due_date} = req.body;
+    const taskData = {name, status, due_date};
+    db.run('INSERT INTO tasks (name, status, due_date) VALUES (?, ?, ?)', [name, status, due_date], function (err) {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.status(201).json(taskData);
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
