f2933c7a8520275dc02a03f02b2bf7100547a935
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-14 19:16:56 -0300

    criação da rota para obter todas as tarefas

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('kanban.db', (err) => {
+const db = new sqlite3.Database('./db/kanban.db', (err) => {
     if (err) {
         console.error('Erro ao abrir o banco de dados:', err.message);
     } else {
@@ -10,13 +10,18 @@ const db = new sqlite3.Database('kanban.db', (err) => {
 
 db.serialize(() => {
     db.run(`
-    CREATE TABLE IF NOT EXISTS tasks (
-      id INTEGER PRIMARY KEY,
-      name TEXT NOT NULL,
-      status TEXT NOT NULL,
-      due_date DATE
-    )
-  `);
+        CREATE TABLE IF NOT EXISTS tasks
+        (
+            id       INTEGER PRIMARY KEY,
+            name     TEXT NOT NULL,
+            status   TEXT NOT NULL,
+            due_date DATE
+        )
+    `);
+
+    db.run("INSERT INTO tasks (name, status, due_date) VALUES ('Tarefa 1', 'To Do', '2024-02-14')");
+    db.run("INSERT INTO tasks (name, status, due_date) VALUES ('Tarefa 2', 'Doing', '2024-02-15')");
+    db.run("INSERT INTO tasks (name, status, due_date) VALUES ('Tarefa 3', 'Ready', '2024-02-16')");
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const db = require('./db/db');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Servidor do Kanban está rodando!');
+app.get('/tasks', (req, res) => {
+    db.all('SELECT * FROM tasks', (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows);
+    });
 });
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
